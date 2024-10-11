dfe522a8b153f7247904f70dcd4c31af5c9d0fe4
Appleblue17 <hluo262@connect.hkust-gz.edu.cn>
2024-10-11 21:59:50 +0800

    Add files: add a test non-tail-if2

--- test/test.sh
+++ test/test.sh
@@ -1,2 +1,1 @@
-moon run src/bin/main.mbt -- --start-stage typecheck --end-stage typecheck ./test/test_after_parsing/fib.json
-W
+moon run src/bin/main.mbt -- --start-stage typecheck --end-stage typecheck ./test/test_after_parsing/non-tail-if2.json
