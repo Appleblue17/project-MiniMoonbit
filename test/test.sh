c2593647c6beaeea660babf7667cc326f390a242
Appleblue17 <hluo262@connect.hkust-gz.edu.cn>
2024-10-23 23:26:01 +0800

    Add files: add test fib_small

--- test/test.sh
+++ test/test.sh
@@ -1,6 +1,6 @@
 start_stage="knf"
 end_stage="knf"
-file_name="ex_frac"
-in_path="./test/test_after_typing/${file_name}.json"
-out_path="./test/test_after_knf/${file_name}.json"
-moon run src/bin/main.mbt -- --json --start-stage $start_stage --end-stage $end_stage $in_path --knf-interpreter
+file_name="non-tail-if2"
+in_path="./test/test_after_parsing/${file_name}.json"
+out_path="./test/test_after_typing/${file_name}.json"
+moon run src/bin/main.mbt -- --start-stage $start_stage --end-stage $end_stage $in_path --knf-interpreter
