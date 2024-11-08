661edd119c8035af69f8914efe5c3421f2978cc1
Appleblue17 <hluo262@connect.hkust-gz.edu.cn>
2024-11-09 01:37:01 +0800

    Bug fixed: knf now handle letrec correctly.

--- test/test_scripts/test_knf.sh
+++ test/test_scripts/test_knf.sh
@@ -1,8 +1,8 @@
 start_stage="knf"
 end_stage="knf"
-file_name="matmul"
+file_name="matmul-flat"
 in_path="./test/test_after_typing/${file_name}.json"
 out_path="./test/test_after_knf/${file_name}.json"
 # moon run src/bin/main.mbt -- --start-stage $start_stage --end-stage $end_stage $in_path
-moon run src/bin/main.mbt -- --json --start-stage $start_stage --end-stage $end_stage $in_path -o $out_path
+# moon run src/bin/main.mbt -- --json --start-stage $start_stage --end-stage $end_stage $in_path -o $out_path
 # moon run src/bin/main.mbt -- --start-stage $start_stage --end-stage $end_stage $in_path --knf-interpreter
