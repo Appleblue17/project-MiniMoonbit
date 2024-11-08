8814e3d300e552e398e116c20dd763aeb29190f2
Appleblue17 <hluo262@connect.hkust-gz.edu.cn>
2024-11-08 21:19:54 +0800

    Bug fixed: typing now can correctly match tuple length.

--- test/test_scripts/test_typecheck.sh
+++ test/test_scripts/test_typecheck.sh
@@ -1,6 +1,6 @@
 start_stage="typecheck"
 end_stage="typecheck"
-file_name="adder"
+file_name="cls-reg-bug"
 in_path="./test/test_after_parsing/${file_name}.json"
 out_path="./test/test_after_typing/${file_name}.json"
 moon run src/bin/main.mbt -- --json --start-stage $start_stage --end-stage $end_stage $in_path -o $out_path
