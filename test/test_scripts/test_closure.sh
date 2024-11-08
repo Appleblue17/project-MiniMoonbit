907320ae5f14553dc5f25cea046d9cda6d83dd0b
Appleblue17 <hluo262@connect.hkust-gz.edu.cn>
2024-11-09 01:19:01 +0800

    Bug fixed: Let now has four parameters (including the rest)

--- test/test_scripts/test_closure.sh
+++ test/test_scripts/test_closure.sh
@@ -1,6 +1,6 @@
 start_stage="closure"
 end_stage="closure"
-file_name="fib_small"
+file_name="matmul"
 in_path="./test/test_after_knf/${file_name}.json"
 out_path="./test/test_after_closure/${file_name}.json"
 # moon run src/bin/main.mbt -- --start-stage $start_stage --end-stage $end_stage $in_path
--- test/test_scripts/test_typecheck.sh
+++ test/test_scripts/test_typecheck.sh
@@ -3,4 +3,5 @@ end_stage="typecheck"
 file_name="matmul"
 in_path="./test/test_after_parsing/${file_name}.json"
 out_path="./test/test_after_typing/${file_name}.json"
+# moon run src/bin/main.mbt -- --json --start-stage $start_stage --end-stage $end_stage $in_path
 moon run src/bin/main.mbt -- --json --start-stage $start_stage --end-stage $end_stage $in_path -o $out_path
