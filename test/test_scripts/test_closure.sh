a8db1292a3e44981fc40d51c65a48bd2baafa1ca
Appleblue17 <hluo262@connect.hkust-gz.edu.cn>
2024-11-09 22:41:53 +0800

    Feature: part risc-v framework.

--- test/test_scripts/test_closure.sh
+++ test/test_scripts/test_closure.sh
@@ -5,4 +5,4 @@ in_path="./test/test_after_knf/${file_name}.json"
 out_path="./test/test_after_closure/${file_name}.json"
 # moon run src/bin/main.mbt -- --start-stage $start_stage --end-stage $end_stage $in_path
 # moon run src/bin/main.mbt -- --json --start-stage $start_stage --end-stage $end_stage $in_path -o $out_path
-# moon run src/bin/main.mbt -- --start-stage $start_stage --end-stage $end_stage $in_path --closure-interpreter
+moon run src/bin/main.mbt -- --start-stage $start_stage --end-stage $end_stage $in_path --closure-interpreter
