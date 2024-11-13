d000a3063b1170a8794a70dd35e53bf74810cc30
Appleblue17 <hluo262@connect.hkust-gz.edu.cn>
2024-11-14 01:13:44 +0800

    [fix] small adjust

--- test/test_scripts/test_asm.sh
+++ test/test_scripts/test_asm.sh
@@ -6,5 +6,5 @@ out_path="./test/test_after_asm/${file_name}.json"
 
 # moon run src/bin/main.mbt -- --start-stage $start_stage --end-stage $end_stage $in_path
 moon run src/bin/main.mbt -- --json --start-stage $start_stage --end-stage $end_stage $in_path -o $out_path
-zig build-exe -target riscv64-linux -femit-bin=$file_name $out_path /runtime/riscv_rt/zig-out/lib/libmincaml.a -O Debug -fno-strip -mcpu=baseline_rv64
-rvlinux -n $file_name
+# zig build-exe -target riscv64-linux -femit-bin=$file_name $out_path /runtime/riscv_rt/zig-out/lib/libmincaml.a -O Debug -fno-strip -mcpu=baseline_rv64
+# rvlinux -n $file_name
